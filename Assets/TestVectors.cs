b8d20d6768f96465d3a4f3dfd88921423b05e9d0
BenjaminLM95 <benjamin.lerner95@gmail.com>
2024-11-04 12:05:16 -0400

    Distance and range condition

--- Assets/TestVectors.cs
+++ Assets/TestVectors.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class TestVectors : MonoBehaviour
 {
     public Transform otherObject;
-
+    public float range = 20.0f; 
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +20,29 @@ public class TestVectors : MonoBehaviour
     private void OnDrawGizmos()
     {
         //Gizmos().DrawLine(Vector.zero, transform.position.normalized);
-        /*Vector3 directionTo = (transform.position - otherObject.transform.position);
-        otherObject.transform.position = directionTo; */
+        Vector3 directionTo = (transform.position - otherObject.transform.position);
+        //otherObject.transform.position = directionTo; */
+
         Vector3 targetPosition = new Vector3(otherObject.position.x, transform.position.y, otherObject.position.z);
         transform.LookAt(targetPosition);
 
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(Vector3.zero, (transform.position - otherObject.transform.position));
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(Vector3.zero, (otherObject.transform.position - transform.position));
+        Gizmos.DrawLine(transform.position, transform.position + directionTo.normalized);
+      
 
+        float distance = directionTo.magnitude;       // Debug.Log(distance); 
+        
+        if(distance > range) 
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, otherObject.transform.position);
+        }
+        else 
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, otherObject.transform.position);
+        }
+         
     }
 
     public void OnGUI()
